00f857c70b2cf2b871756063716085ad0a5b29e1
Rainir <off_rainir@proton.me>
2023-02-19 19:17:58 +0300

    Minor fixes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,5 @@
 package org.example;
 
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.Random;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -18,11 +16,23 @@ public class Main {
             texts[i] = generateText("abc", 3 + random.nextInt(3));
         }
 
-        Thread thread1 = new Thread(() -> isPalindrome(texts));
+        Thread thread1 = new Thread(() -> {
+            for (String value : texts) {
+                if (isPalindrome(value) && !isOneLitter(value)) incrementCounter(value.length());
+            }
+        });
 
-        Thread thread2 = new Thread(() -> isOneLitter(texts));
+        Thread thread2 = new Thread(() -> {
+            for (String value : texts) {
+                if (isOneLitter(value)) incrementCounter(value.length());
+            }
+        });
 
-        Thread thread3 = new Thread(() -> isAscendingLetter(texts));
+        Thread thread3 = new Thread(() -> {
+            for (String value : texts) {
+                if (isAscendingLetter(value) && !isOneLitter(value)) incrementCounter(value.length());
+            }
+        });
 
         thread1.start();
         thread1.join();
@@ -45,112 +55,36 @@ public class Main {
         return text.toString();
     }
 
-    public static void isPalindrome(String[] texts) {
-        String[] values = new String[10];
-        int counter = 0;
-        for (String value : texts) {
-            String[] text = value.split("");
-            String[] textContrary = text.clone();
-            Collections.reverse(Arrays.asList(textContrary));
-            if (Arrays.equals(text, textContrary)) {
-                if (counter < 10) {
-                    values[counter] = value;
-                    counter++;
-                }
-                if (text.length == 3) {
-                    counterThree.incrementAndGet();
-                } else if (text.length == 4) {
-                    counterFour.incrementAndGet();
-                } else if (text.length == 5) {
-                    counterFive.incrementAndGet();
-                }
-            }
-        }
-        System.out.println("Примеры слов палиндромов:");
-        System.out.println(Arrays.toString(values) + "\n");
+    public static boolean isPalindrome(String value) {
+        return value.equals(new StringBuilder(value).reverse().toString());
     }
 
-    public static void isOneLitter(String[] texts) {
-        String[] values = new String[10];
-        int counter = 0;
-        for (String value : texts) {
-            String[] text = value.split("");
-            for (int i = 0; i < text.length; i++) {
-                if(text[0].equals(text[i])) {
-                    if (i == text.length - 1) {
-                        if (counter < 10) {
-                            values[counter] = value;
-                            counter++;
-                        }
-                        if (text.length == 3) {
-                            counterThree.incrementAndGet();
-                        } else if (text.length == 4) {
-                            counterFour.incrementAndGet();
-                        } else if (text.length == 5) {
-                            counterFive.incrementAndGet();
-                        }
-                    }
-                } else {
-                    break;
-                }
+    public static boolean isOneLitter(String value) {
+        String[] text = value.split("");
+        for (int i = 1; i < text.length; i++) {
+            if(!text[0].equals(text[i])) {
+                return false;
             }
         }
-        System.out.println("Примеры слов состоящих из одной буквы:");
-        System.out.println(Arrays.toString(values) + "\n");
+        return true;
     }
 
-    public static void isAscendingLetter(String[] texts) {
-        String[] values = new String[10];
-        int counter = 0;
-        for (String value : texts) {
-            String[] text = value.split("");
-            breakPoint:
-            for (int i = 0; i < text.length; i++) {
-                if (i < text.length - 1) {
-                    switch (text[i]) {
-                        case "a" -> {
-                            if (!text[i + 1].equals("a")) {
-                                continue;
-                            } else if (!text[i + 1].equals("b")) {
-                                continue;
-                            } else if (!text[i + 1].equals("c")) {
-                                continue;
-                            }
-                        }
-                        case "b" -> {
-                            if (text[i + 1].equals("a")) {
-                                break breakPoint;
-                            } else if (!text[i + 1].equals("b")) {
-                                continue;
-                            } else if (!text[i + 1].equals("c")) {
-                                continue;
-                            }
-                        }
-                        case "c" -> {
-                            if (!text[i + 1].equals("c")) {
-                                break breakPoint;
-                            }
-                        }
-                        default -> {
-                            continue;
-                        }
-                    }
-                } else if (i == text.length - 1) {
-                    if (counter < 10) {
-                        values[counter] = value;
-                        counter++;
-                    }
-                    if (text.length == 3) {
-                        counterThree.incrementAndGet();
-                    } else if (text.length == 4) {
-                        counterFour.incrementAndGet();
-                    } else if (text.length == 5) {
-                        counterFive.incrementAndGet();
-                    }
-                }
+    public static boolean isAscendingLetter(String value) {
+        for (int i = 1; i < value.length(); i++) {
+            if (value.charAt(i) < value.charAt(i - 1)) {
+                return false;
             }
         }
-        System.out.println("Примеры слов с буквами по возрастанию:");
-        System.out.println(Arrays.toString(values) + "\n");
+        return true;
+    }
+
+    public static void incrementCounter(int textLength) {
+        if (textLength == 3) {
+            counterThree.incrementAndGet();
+        } else if (textLength == 4) {
+            counterFour.incrementAndGet();
+        } else if (textLength == 5) {
+            counterFive.incrementAndGet();
+        }
     }
 }
